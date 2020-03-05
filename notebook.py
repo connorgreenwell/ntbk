8ced54e2e6f1ac53347e2e2ffa40932684e3b8d4
Connor Greenwell <connor@mint-julep.local>
2020-03-05 08:47:03 -0500

    initial commit

---
+++ notebook.py
@@ -0,0 +1,76 @@
+import sys, tempfile, os
+from subprocess import call
+from fuzzywuzzy import process
+
+import logging
+logging.disable()
+
+EDITOR = os.environ.get('EDITOR', 'vim')
+SEPARATOR = "\n/////////////////////////////\n"
+
+class Entry():
+	def __init__(self, raw_txt):
+		self.body = raw_txt
+		self.title = raw_txt.split("\n", 1)[0]
+
+def ls(entries, args):
+	results = process.extract(args.title, entries.keys(),
+		limit=args.n)
+	for idx, (entry, score) in enumerate(results):
+		print("[{:3d}] {}".format(idx, entry))
+
+def edit(entries, args):
+	title, score = process.extractOne(args.title, entries.keys())
+
+	if score > 95:
+		entry = entries[title]
+	else:
+		title = args.title
+		underline = "=" * len(args.title)
+		entry = Entry("{}\n{}\n".format(args.title, underline))
+
+	initial_message = entry.body
+
+	with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
+		tf.write(initial_message.encode())
+		tf.flush()
+		call([EDITOR, '+set backupcopy=yes', tf.name])
+		
+		# do the parsing with `tf` using regular File operations.
+		# for instance:
+		tf.seek(0)
+		edited_message = tf.read().decode("utf-8")
+
+		entries[title] = Entry(edited_message)
+
+# TODO: delete
+# TODO: cat
+
+if __name__ == "__main__":
+	import argparse
+	parser = argparse.ArgumentParser()
+	subparsers = parser.add_subparsers()
+	parser.add_argument("--notebook_path", type=str, default="./notes.txt")
+
+	edit_parser = subparsers.add_parser("edit")
+	edit_parser.add_argument("title", type=str)
+	edit_parser.set_defaults(func=edit)
+
+	ls_parser = subparsers.add_parser("ls")
+	ls_parser.add_argument("title", nargs="?", default=" ")
+	ls_parser.add_argument("-n", default=None, type=int)
+	ls_parser.set_defaults(func=ls)
+
+	args = parser.parse_args()
+
+	with open(args.notebook_path, "r+") as f:
+		data = f.read()
+		entries = {
+			entry.title: entry
+			for entry in map(Entry, data.split(SEPARATOR))
+		}
+		args.func(entries, args)
+
+		f.seek(0)
+		f.write(SEPARATOR.join([entry.body for entry in entries.values()]))
+		f.truncate()
